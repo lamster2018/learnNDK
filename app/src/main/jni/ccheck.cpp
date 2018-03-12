bec29971fa19499a6002e17655e603de83d1c9d6
lahm <l292538769@gmail.com>
2018-03-12 16:45:32 +0800

    remove check traceid from ccheck

--- app/src/main/jni/ccheck.cpp
+++ app/src/main/jni/ccheck.cpp
@@ -85,66 +85,6 @@ isEquals(JNIEnv *env, jobject obj, jstring str) {
     }
 }
 
-//获取TracePid
-int get_number_for_str(char *str) {
-    if (str == NULL) {
-        return -1;
-    }
-    char result[20];
-    int count = 0;
-    while (*str != '\0') {
-        if (*str >= 48 && *str <= 57) {
-            result[count] = *str;
-            count++;
-        }
-        str++;
-    }
-    int val = atoi(result);
-    return val;
-}
-
-//开启循环轮训检查TracePid字段
-void *thread_function(void *argv) {
-    int pid = getpid();
-    char file_name[20] = {'\0'};
-    sprintf(file_name, "/proc/%d/status", pid);
-    char linestr[256];
-    int i = 0, traceid;
-    FILE *fp;
-    while (1) {
-        i = 0;
-        fp = fopen(file_name, "r");
-        if (fp == NULL) {
-            break;
-        }
-        while (!feof(fp)) {
-            fgets(linestr, 256, fp);
-            if (i == 5) {
-                traceid = get_number_for_str(linestr);
-                LOGD("traceId:%d", traceid);
-                if (traceid > 0) {
-                    LOGD("I was be traced...trace pid:%d", traceid);
-                    //华为P9会主动给app附加一个进程
-                    exit(0);
-                }
-                break;
-            }
-            i++;
-        }
-        fclose(fp);
-        sleep(5);
-    }
-    return ((void *) 0);
-}
-
-void create_thread_check_traceid() {
-    pthread_t t_id;
-    int err = pthread_create(&t_id, NULL, thread_function, NULL);
-    if (err != 0) {
-        LOGD("create thread fail: %s\n", strerror(err));
-    }
-}
-
 /**
  * 签名校验函数
  */
@@ -200,11 +140,8 @@ JNI_OnLoad(JavaVM *vm, void *reserved) {
     LOGD("JNI on load...");
 
     //自己附加
-    LOGD("ptrace myself...");
+//    LOGD("ptrace myself...");
 //    ptrace(PTRACE_TRACEME, 0, 0, 0);
-
-    //检测自己有没有被trace
-    create_thread_check_traceid();
 
     //声明变量
     jint result = JNI_ERR;
